8ab58e5b5e89bb4d3f9f43dce2eaa8aba5a84416
trachcha <trachcha@fit.cvut.cz>
2026-06-28 13:55:55 +0200

    Use models in POST /tasks

--- app/main.py
+++ app/main.py
@@ -1,8 +1,35 @@
 from fastapi import FastAPI
 
+from models.task import Task
+from models.task import TaskRequest
+
 app = FastAPI()
+
+tasks: list[Task] = []
+id_counter = 1
 
 
 @app.get("/")
 async def root():
     return {"message": "StudentTask API is running."}
+
+
+@app.post("/tasks")
+def create_task(request: TaskRequest):
+    global id_counter
+
+    new_task = Task(
+        id=id_counter,
+        title=request.title
+    )
+
+    id_counter += 1
+
+    tasks.append(new_task)
+
+    return new_task
+
+
+@app.get("/tasks")
+def get_all_tasks():
+    return tasks
